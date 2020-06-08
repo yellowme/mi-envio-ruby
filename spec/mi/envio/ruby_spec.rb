824e18b2c79eeec33e921b4a76b2c190b849442f
Gabriel Dzul <gabrieldzul@gmail.com>
2020-06-08 12:09:35 -0500

    removed unused files

--- spec/mi/envio/ruby_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe Mi::Envio::Ruby do
-  it "has a version number" do
-    expect(Mi::Envio::Ruby::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
