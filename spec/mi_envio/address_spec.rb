20f64438acd1bbad20f8b7cd57dbc496dc9f8819
Gabriel Dzul <gabrieldzul@gmail.com>
2020-06-09 11:26:28 -0500

    Added failing test case

--- spec/mi_envio/address_spec.rb
+++ spec/mi_envio/address_spec.rb
@@ -22,11 +22,45 @@ RSpec.describe MiEnvio do
     end
     
 
-    it 'Generates an order in the eship panel' do
+    it 'Generates an address' do
       response = MiEnvio.post_address(payload)
-      p response['address'].inspect
+      p response['address']['name']
+      p payload[:name]
       #Check origin address
-      expect(parsed_payload['address_from']['name']).to eq response['address_from']['name']
+      expect(payload[:name]).to eq response['address']['name']
+      expect(payload[:street]).to eq response['address']['street']
+      expect(payload[:street2]).to eq response['address']['street2']
+      expect(payload[:zipcode]).to eq response['address']['zipcode']
+      expect(payload[:email]).to eq response['address']['email']
+      expect(payload[:phone]).to eq response['address']['phone']
+    end
+  end
+
+  context 'With invalid payload' do
+
+    let(:payload){
+      {
+        "object_type":"PURCHASE",
+        "name":"Juan Ramon2",
+        "street":"Pocito 150",
+        "street2":"Popotla",
+        "reference":"Casa blanca portón negro",
+        "country":"MX",
+        "zipcode":11400,
+        "email":"dev@mienvio.mx",
+        "phone":"4771102332",
+      }
+    }
+
+    before do
+      WebMock.allow_net_connect!
+    end
+    
+
+    it 'returns a 422 status code' do
+      response = MiEnvio.post_address(payload)
+      expect(response.code).to eq (422)
+      expect(response.message).not_to be nil
     end
   end
 
