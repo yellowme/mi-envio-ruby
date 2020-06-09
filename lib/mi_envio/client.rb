52dcdc5fcde7a3f77226b36240009463a72065a9
Gabriel Dzul <gabrieldzul@gmail.com>
2020-06-09 11:26:02 -0500

    Added client error rescue

--- lib/mi_envio/client.rb
+++ lib/mi_envio/client.rb
@@ -1,8 +1,14 @@
 module MiEnvio
   module Client
     def self.post(path:, body:, headers: {})
-      response = MiEnvio.connection.post path, body.to_json, headers
-      JSON.parse(response.body)
+      begin
+        response = MiEnvio.connection.post path, body.to_json, headers
+        JSON.parse(response.body)
+      rescue Faraday::ClientError => e
+        raise MiEnvio::Error.new(message: e.response[:body], code: e.response[:status])
+      end
     end
   end
 end
+
+      
